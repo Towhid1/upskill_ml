5dc33d75435066087b4fdc112a5065e13fc9c784
Towhid1 <ntowhid@coel.run>
2020-08-30 18:12:42 +0600

    day3 4

---
+++ day3&4/module.py
@@ -0,0 +1,18 @@
+def calculate_sum(val1, val2):
+    '''
+    Calculate sum of val1 and val2
+
+    Parameters
+    ----------
+    val1 : int/float
+        First value
+    val1 : int/float
+        Second value
+
+    Returns
+    -------
+    int/float
+        sum of val1 and val2
+    '''
+    result = val1 + val2
+    return result
